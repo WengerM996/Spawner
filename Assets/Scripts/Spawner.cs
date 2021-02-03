89a50d5be75fdb57fa6104f30c0e6a7141b29abb
WengerM996 <maks.wenger@yandex.ru>
2021-02-03 11:39:16 +0300

    init

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Serializable]
+    private struct SpawnObject
+    {
+        [SerializeField] private KeyCode _key;
+        [SerializeField] private GameObject _gameObject;
+        [SerializeField] private Transform _spawnPoint;
+
+        public KeyCode GetKey() { return _key; }
+        public GameObject GetObject() { return _gameObject; }
+        public Vector3 GetPosition() { return _spawnPoint.position; }
+    }
+    
+    [SerializeField] private SpawnObject[] _spawnObjects;
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        foreach (var spawnObject in _spawnObjects)
+        {
+            if (Input.GetKeyDown(spawnObject.GetKey()))
+            {
+                Create(spawnObject.GetObject(), spawnObject.GetPosition());
+            }
+        }
+    }
+
+    private void Create(GameObject spawnObject, Vector3 position)
+    {
+        Instantiate(spawnObject, position, Quaternion.identity);
+    }
+}
